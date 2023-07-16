b57e92f30a545a37b759a7a3e61fc397629acfea
Preston-D <prestonduffield1@gmail.com>
2023-07-16 12:29:55 -0700

    feat: truncated description

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,13 @@
 class Photo < ApplicationRecord
   has_one_attached :image
+
+  TRUNCATED_DESCRIPTION_MAX_LENGTH = 50
+
+  def truncated_description
+    if description.length > TRUNCATED_DESCRIPTION_MAX_LENGTH
+      description[0..(TRUNCATED_DESCRIPTION_MAX_LENGTH - 4)] + '...'
+    else
+      description
+    end
+  end
 end
