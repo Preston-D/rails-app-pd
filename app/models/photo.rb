4731122fd36078f289a65eb68d47b45516ffae68
Preston-D <prestonduffield1@gmail.com>
2023-07-18 11:54:15 -0700

    feat: photo size variants

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,17 @@
 class Photo < ApplicationRecord
-  has_one_attached :image
-
   TRUNCATED_DESCRIPTION_MAX_LENGTH = 50
+
+  THUMB_SIZE = 100
+  SMALL_SIZE = 400
+  MEDIUM_SIZE = 800
+  LARGE_SIZE = 1200
+
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [THUMB_SIZE, THUMB_SIZE]
+    attachable.variant :small, resize_to_limit: [SMALL_SIZE, SMALL_SIZE]
+    attachable.variant :medium, resize_to_limit: [MEDIUM_SIZE, MEDIUM_SIZE]
+    attachable.variant :large, resize_to_limit: [LARGE_SIZE, LARGE_SIZE]
+  end
 
   def truncated_description
     if description.length > TRUNCATED_DESCRIPTION_MAX_LENGTH
@@ -9,5 +19,26 @@ class Photo < ApplicationRecord
     else
       description
     end
+  end
+
+  # Vips documentation: https://www.rubydoc.info/gems/ruby-vips/Vips
+  def image_thumb
+    image.variant(:thumb)
+  end
+
+  def image_small
+    image.variant(:small)
+  end
+
+  def image_medium
+    image.variant(:medium)
+  end
+
+  def image_large
+    image.variant(:large)
+  end
+
+  def image_full
+    image
   end
 end
